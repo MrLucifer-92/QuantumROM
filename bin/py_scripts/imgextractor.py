44342ccf3c85a2ac5ef6ece326f7c606c540d4d7
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2026-08-29 22:12:21 -0700

    fix ext4 img extract for no-sudo user

--- bin/py_scripts/imgextractor.py
+++ bin/py_scripts/imgextractor.py
@@ -186,7 +186,11 @@ class Extractor(object):
                         os.makedirs(dir_target)
                     if os.name == 'posix':
                         os.chmod(dir_target, int(mode, 8))
-                        os.chown(dir_target, uid, gid)
+                        try:
+                            os.chown(dir_target, uid, gid)
+                        except:
+                            print(f"Failed to set permission for: {dir_target}")
+                            pass
                     scan_dir(entry_inode, entry_inode_path)
                     if cap == '' and con == '':
                         tmppath=self.FileName + entry_inode_path
@@ -278,7 +282,11 @@ class Extractor(object):
                         with open(file_target, 'wb') as out:
                             out.write(raw)
                         os.chmod(file_target, int(mode, 8))
-                        os.chown(file_target, uid, gid)
+                        try:
+                            os.chown(file_target, uid, gid)
+                        except:
+                            print(f"Failed to set permission for: {file_target}")
+                            pass
                     if cap == '' and con == '':
                         tmppath=self.FileName + entry_inode_path
                         if (tmppath).find(' ',1,len(tmppath))>0:
